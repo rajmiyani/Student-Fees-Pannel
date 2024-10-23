5230c2ba120990d7a9afb1563212eee5f49a3cca
rajmiyani <rajmiyani06@gmail.com>
2024-10-23 17:30:22 +0530

    changes in mongoose

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require ('mongoose')
 var db = mongoose.connection
+mongoose.connect('mongodb+srv://rajmiyani06:DzNwSyXWRHM9avbC@cluster0.rb5rcbc.mongodb.net/nodejs-project')
 
-mongoose.connect('mongodb://127.0.0.1/student-portal')
+// mongoose.connect('mongodb://127.0.0.1/student-portal')
 
 db.once("open",(err)=>{
     if(err){
